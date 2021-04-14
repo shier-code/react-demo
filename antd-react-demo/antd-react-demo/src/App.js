1e217df96c54463c1afcb4910fee37fdcd9961f1
wentan <2486178195@qq.com>
2021-04-14 10:29:54 +0800

    111

---
+++ antd-react-demo/antd-react-demo/src/App.js
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion: 
+ * @version: v3.10
+ * @Author: wentan
+ * @Date: 2021-04-14 09:49:40
+ * @LastEditors: wentan
+ * @LastEditTime: 2021-04-14 09:59:47
+ */
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
